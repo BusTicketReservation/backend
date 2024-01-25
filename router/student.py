6ea64f1d1ba80da70b57f54c87e6e58557bc91d3
thebbyo <dibbyoroy7@gmail.com>
2024-01-25 22:14:17 +0600

    student profile

--- router/student.py
+++ router/student.py
@@ -12,7 +12,7 @@ router = APIRouter(
 
 @router.get("/student/profile", response_model=schemas.Student)
 def getStudentProfile(db: Session = Depends(database.get_db), currentUser = Depends(oauth2.getCurrentUser)):
-    if currentUser.role != "STUDENT":
+    if currentUser.role != "STUDENT"  and db.query(models.Student).filter(models.Student.email != currentUser.email).first():
         raise HTTPException(status_code=400, detail="error")
     student = db.query(models.Student).filter(models.Student.email == currentUser.email).first()
     return student
