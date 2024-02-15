45c77bb926e9285e05661ef6c5e5a2bca371d7a7
thebbyo <dibbyoroy7@gmail.com>
2024-02-15 21:59:46 +0600

    fixing errorrrr

--- router/founder.py
+++ router/founder.py
@@ -225,9 +225,10 @@ def deleteCourse(courseID: int, db: Session = Depends(database.get_db), currentU
 
     course = db.query(models.Courses).filter(
         models.Courses.id == courseID).first()
-    if not course:
-        raise HTTPException(status_code=400, detail="courseError")
     
+    if not course:
+        
+        raise HTTPException(status_code=400, detail="courseError")
     
 
     courseTeacher = db.query(models.CourseTeacher).filter(
